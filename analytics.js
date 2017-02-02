828df3903c957c1b2b3bbe0341ac9d72c06aa951
vuurball <oneindigedroom@gmail.com>
2017-02-02 17:05:18 +0200

    removed code

--- analytics.js
+++ analytics.js
@@ -1,21 +1,10 @@
 var analytics = new Firebase('https://olgabdb-f2ace.firebaseio.com/');
 var activeVisitors = analytics.child('activeVisitors');
 
-
-// activeVisitors.push({
-//   path: window.location.pathname,
-//   arrivedAt: Firebase.ServerValue.TIMESTAMP,
-//   userAgent: navigator.userAgent
-// });
-
-
-
-
 var totalVisitors = analytics.child('totalVisitors');
 totalVisitors.transaction(function (currentData) {
   return currentData + 1;
 });
-
 
 var visitor = {
   path: window.location.pathname,
